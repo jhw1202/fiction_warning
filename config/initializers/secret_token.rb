9e9b11fcce740459c5e5ce101fc92f07e80c5e66
Gavin St. Ours <charmcitygavin@gmail.com>
2013-04-21 11:16:10 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FictionWarning::Application.config.secret_token = 'cb104eb3de18f9c2e1d43bd145dd5f05342b3e6e5ce5976784ebb02c110794262876480cee428c482ac510512fbaa6f47d3312d6795e4824cf28a555f27d08e1'
