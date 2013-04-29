f62b18811a6071195d192a1a8d94792d3119cc70
Gavin St. Ours <charmcitygavin@gmail.com>
2013-04-29 11:57:21 -0700

    Clarify terms and create basic text box styling.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def not_found
     raise ActionController::RoutingError.new('Not Found')
   end
+
+  def current_page
+    Page.find_by_id(params[:id])
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def show
-    @page  = Page.find_by_id(params[:id])
+    @page  = current_page
     @title = @page.title
     @choices = Choice.where("page_id = ?", params[:id])
+  end
+
+  def update
+    @page  = current_page
+    puts "***** #{@page.title}"
+    redirect_to "pages/#{@page.id}"
   end
 end
