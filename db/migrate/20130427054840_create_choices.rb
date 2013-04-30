714c637347a9229ca9a2e1e92c34b28c8c5aa020
Gavin St. Ours <charmcitygavin@gmail.com>
2013-04-30 15:40:17 -0700

    Removed title string.

--- db/migrate/20130427054840_create_choices.rb
+++ db/migrate/20130427054840_create_choices.rb
@@ -1,7 +1,6 @@
 class CreateChoices < ActiveRecord::Migration
   def change
     create_table :choices do |t|
-      t.string     :title
       t.integer    :child_id
       t.references :page
       t.timestamps
