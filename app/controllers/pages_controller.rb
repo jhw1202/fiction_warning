c0470330bd76a984ebd841ae3f5652d9b166c4b8
Gavin St. Ours <charmcitygavin@gmail.com>
2013-04-30 15:34:37 -0700

    Move page and choice creation logic out of view.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,30 @@
 class PagesController < ApplicationController
   def show
-    @page  = current_page
+    @page = current_page
     @title = @page.title
     @choices = Choice.where("page_id = ?", params[:id])
+    if @page.content.blank?
+      render 'pages/edit'
+    end
+  end
+
+  def edit
+    @page = current_page
   end
 
   def update
-    @page  = current_page
-    @page.choices.build
-    redirect_to "pages/#{@page.id}"
+    @page = current_page
+    @page.update_attributes(:content => params[:content])
+    @page.save
+
+    @page1 = Page.create(:title => params[:choice1])
+    @page2 = Page.create(:title => params[:choice2])
+    @page3 = Page.create(:title => params[:choice3])
+
+    @choice1 = Choice.create(:child_id => @page1.id, :page_id => @page.id)
+    @choice2 = Choice.create(:child_id => @page2.id, :page_id => @page.id)
+    @choice3 = Choice.create(:child_id => @page3.id, :page_id => @page.id)
+
+    redirect_to @page
   end
 end
